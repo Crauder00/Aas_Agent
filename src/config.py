9e4d50301516fc1ef1f1d83923d2a49b7e2b479a
Crauder@Home <simonfritschi@hotmail.com>
2026-03-24 18:38:52 +0100

    Refactor MQTT configuration and event listener to support multiple topic filters

--- src/config.py
+++ src/config.py
@@ -1,18 +1,26 @@
 from dataclasses import dataclass
-
+from typing import List, Optional
 
 @dataclass
 class MqttConfig:
-    host: str = "localhost"
+    host: str = "192.168.1.128"
     port: int = 1883
     # Topic-Pattern vom BaSyx Server
-    topic_filter: str = "sm-repository/+/submodels/+/submodelElements/+/updated"
+    # topic_filter: str = "sm-repository/sm-repo/submodels/aHR0cHM6Ly9leGFtcGxlLmNvbS9pZHMvc20vNTY1MF85MTM0XzQ5MzhfNDM0MA/submodelElements/Operations[0]/updated"
+    topic_filters: Optional[List[str]] = None
     client_id: str = "aas-agent"
+
+    def __post_init__(self):
+        if self.topic_filters is None:
+            self.topic_filters = [
+                "sm-repository/sm-repo/submodels/aHR0cHM6Ly9leGFtcGxlLmNvbS9pZHMvc20vNTY1MF85MTM0XzQ5MzhfNDM0MA/submodelElements/Operations[0]/updated",
+                "sm-repository/sm-repo/submodels/aHR0cHM6Ly9leGFtcGxlLmNvbS9pZHMvc20vNTY1MF85MTM0XzQ5MzhfNDM0MA/submodelElements/Operations[1]/updated",
+            ]
 
 
 @dataclass
 class AasConfig:
-    base_url: str = "http://localhost:8080"
+    base_url: str = "http://192.168.1.128:8080"
     # Submodel-ID deines Emission-Submodels (Base64URL-encoded)
     emission_submodel_id: str = "your-submodel-id-base64"
 
